40c5651ab73201597302419277773f312a71968d
crex02 <127298119+Pentakilj97@users.noreply.github.com>
2023-05-23 12:59:46 +0200

    ok

--- src/app/services/reddit-service/reddit.service.ts
+++ src/app/services/reddit-service/reddit.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import { Post } from 'src/app/model/post';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,16 @@ import { Injectable } from '@angular/core';
 export class RedditService {
 
   constructor(private http: HttpClient) { }
-  getRedditPosts(argument:string){
-    return this.http.get<any>('https://www.reddit.com/r/'+argument+'/hot.json?limit=100')
+  getRedditPosts(argument:string):Observable<Post[]>{
+    return this.http.get<any>('https://www.reddit.com/r/'+argument+'/hot.json?limit=100').pipe(
+      tap((obj)=>console.log("sono dentro il tap",obj)),
+      map((obj)=>obj.data),
+      tap((data)=>console.log('sono dentro il secondo tap',data)),
+      map((data)=> data.children),
+      tap((children)=>console.log('sono dentro il terzo tap',children)),
+      map((children)=>children.map((child:any)=>child.data)),
+      tap((childrenData)=>console.log('sono dentro il quarto tap',childrenData)),
+
+    )
   }
 }
